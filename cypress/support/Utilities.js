36f1f4158342b68b56d6f99eed7f1606ba765632
muhammednalci <muhammed.nalci@gmail.com>
2020-08-13 16:16:32 +0200

    chore(test): move regex def to a separate file

---
+++ cypress/support/Utilities.js
@@ -0,0 +1,7 @@
+
+export default class Utilities {
+    getEmailRegex = () => {
+        var regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return regex;
+    }
+}
